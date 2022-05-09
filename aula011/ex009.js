170d4e04c9a3b167d8b8e2874fc3cc101f7c364d
Rebeca Kethelyn Lopes <rebecasilva1406@gmail.com>
2022-05-09 16:09:33 -0300

    Criação de novos exercicios

---
+++ aula011/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Eua'
+if (pais == 'Brasil'){
+    console.log('voce é brasileiro')
+} else{
+    console.log ('Voce é estranheiro')
+}
---
+++ aula012/ex011.js
@@ -0,0 +1,8 @@
+var idade = 16
+if (idade < 16) {
+    console.log('nao vota')
+} else if (idade < 18 || idade > 65){
+       console.log('voto opcional')
+} else{
+    console.log('voto obrigatorio')
+}
