5af1d3bbfa846640f7e1a1efb93def3e75c3607f
Rebeca Kethelyn Lopes <rebecasilva1406@gmail.com>
2022-05-10 16:18:31 -0300

    Criação do desafio 4

--- desafio4/modelo/script.js
+++ desafio4/modelo/script.js
@@ -1,52 +1,18 @@
-function verificar(){
-    var data= new Date()
-    var ano = data.getFullYear()
-    var fano= document.getElementById('txtano')
-    var res = document.getElementById('res')
+function calcular(){
+    var num = document.getElementById('txtnum')
+    var tab = document.getElementById('seltab')
+    if (num.value.length == 0){
+        window.alert('Por favor, digite um numero')
+    } else{
+        let n = Number(num.value)
+        tab.innerHTML= ''
+        for(var c= 1;c <= 10; c++){
+            let item= document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
 
-   if (fano.value.length == 0 || Number(fano.value) > ano ){
-        window.alert('[ERRO] Verifique os dados e tente novamente')
-   } else {
-       var fsex = document.getElementsByName('radsex')
-       var idade = ano - Number(fano.value)
-       var genero = ''
-       var img = document.createElement('img')
-       img.setAttribute('id', 'foto')
-       if (fsex[0].checked){
-           genero = 'Homem'
-           document.body.style.background= '#a2d6f9'
-           if (idade >= 0 && idade < 14 ){
-               //criança
-               img.setAttribute('src', 'fotos/menino.png')
-           } else if (idade < 30){
-               //jovem
-               img.setAttribute('src', 'fotos/homem-jovem.png')
-           } else if (idade < 50){
-               //adulto
-               img.setAttribute('src', 'fotos/homem-adulto.png')
-           } else {
-               //idoso
-               img.setAttribute('src', 'fotos/homem-idoso.png')
-           }
-       } else if (fsex[1].checked){
-           genero = 'Mulher'
-           document.body.style.background= '#ffcfd2'
-           if (idade >= 0 && idade < 14 ){
-                //criança
-            img.setAttribute('src', 'fotos/menina.png')
-           } else if (idade < 30){
-                //jovem
-                img.setAttribute('src', 'fotos/mulher-jovem.png')
-           } else if (idade < 50){
-                //adulto
-                img.setAttribute('src', 'fotos/mulher-adulta.png')
-           } else {
-                //idoso
-                img.setAttribute('src', 'fotos/mulher-idosa.png')
-           }
-       }
-       res.style.textAlign= 'center'
-       res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-       res.appendChild(img)
-   }
+    }
+    
 }
