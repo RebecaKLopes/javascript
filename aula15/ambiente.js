6a8e4f5e4192165efe50b419960a7fdf933c66ea
Rebeca Kethelyn Lopes <rebecasilva1406@gmail.com>
2022-05-11 12:07:32 -0300

    Criação de novos exercicios

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5,8,9,3]
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(4)
+console.log(`O valor 8 esta na posição ${pos}`)
+
+num.sort()
