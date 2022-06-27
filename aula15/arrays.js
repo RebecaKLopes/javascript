a46b3b03a802b82c8c03b280b5054894b110ab82
Rebeca Kethelyn Lopes <rebecasilva1406@gmail.com>
2022-06-27 12:42:35 -0300

    criação de novos exercícios

---
+++ aula15/arrays.js
@@ -0,0 +1,7 @@
+var num = ["abelha", "colocar", "dado", "cachorro"];
+var numeros = num.join()
+console.log(numeros + "numeros")
+var sorteia = num.sort();
+console.log(sorteia)
+var reverso = num.reverse();
+console.log(reverso)
